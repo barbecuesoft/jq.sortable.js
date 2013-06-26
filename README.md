15fb61c3ed27d32bdac0e6c5b1f89c741456f4af
jacko <fabri@inventati.org>
2013-06-26 17:23:35 +0200

    docs

--- README.md
+++ README.md
@@ -1,13 +1,28 @@
-jq.sortable.js
-==============
+# jq.sortable.js
 
 AppFramework jQMobi scrollable lists plugin
 
 
 
-USAGE
---------------
+# USAGE
+
 Basic usage:
+
+INCLUDE
+====
+
+```html
+    <!-- JS Section -->
+    <script src="jq.mobi.js" type="text/javascript" charset="utf-8"></script>
+    <script src="jq.ui.js" type="text/javascript" charset="utf-8"></script>
+    <script src="jq.sortable.js" type="text/javascript" charset="utf-8"></script>
+
+    <!-- CSS Section -->
+    <link rel="stylesheet" href="jq.sortable.css" type="text/css" media="screen" charset="utf-8">
+```
+
+HTML
+====
 
 ```html
 <ul class='sortable'>
@@ -17,10 +32,35 @@ Basic usage:
   <li>Elem4</li>
   <li>Elem5</li>
 </ul>
+```
 
+JS
+====
+
+```js
 <script type='text/javascript'>
   $(function() {
     $('.sortable').sortable();
   });
 </script>
 ```
+
+OPTIONS
+====
+
+jq.sortable.js takes the following options:
+
+ *  *overlap*: string | class assigned to overlapped li (default: 'ui-overlap')
+ *  *dragged*: string | class assigned to dragged li (default: 'ui-dragged')
+ *  *placeholder*: string | class assigned to placeholder li (default: 'ui-placeholder')
+ *  *relative_to*: string | selector represinting the parent object (default: 'body')
+ *  *before_drag*: function | function called before dragging starts 
+                               dragged int index of the dragged element
+                             overlap int index of the substituted element, -1 if none
+ *  *on_drag*: function | function called during dragging 
+ *                         dragged int index of the dragged element
+ *                         overlap int index of the substituted element, -1 if none
+ *  *after_drag*: function | function called after dragging is end paramenters
+ *                            dragged int index of the dragged element
+ *                            overlap int index of the substituted element, -1 if none
+
--- jq.sortable.js
+++ jq.sortable.js
@@ -1,7 +1,39 @@
 /**
- * jq.sortable.js - a sortable lists library for html5 mobile apps
+ * @author Barbecue Lab // bbqlab.net
+ * @desc a sortable lists plugins for AppFramework jQMobi
+ *
+ *
+ * Include it in your project:
+ *   <script src="jq.sortable.js" type="text/javascript" charset="utf-8"></script>
+ *   stylesrc
+ *
+ * Initialize:
+ *   $('.selector').sortable(options);
+ *
+ *   options is an object with following properties:
+ *     
+ *     overlap: string | class assigned to overlapped li (default: 'ui-overlap')
+ *     dragged: string | class assigned to dragged li (default: 'ui-dragged')
+ *     placeholder: string | class assigned to placeholder li (default: 'ui-placeholder')
+ *     relative_to: string | selector represinting the parent object (default: 'body')
+ *     before_drag: function | function called before dragging starts
+ *                            dragged int index of the dragged element
+ *                            overlap int index of the substituted element, -1 if none
+ *     on_drag: function | function called during dragging 
+ *                         dragged int index of the dragged element
+ *                         overlap int index of the substituted element, -1 if none
+ *     after_drag: function | function called after dragging is end paramenters
+ *                            dragged int index of the dragged element
+ *                            overlap int index of the substituted element, -1 if none
+ *
+ *
+ * Example:
+ *
+ *  $('.selector').sortable({after_drag: function(dragged, overlap) {console.log(dragged);}});
  *
  */
+
+
 (function($) {
     var cache = [];
 
@@ -25,7 +57,10 @@
         }
         return this.length == 1 ? tmp : this;
     };
-
+    
+    /**
+     *  Abstraction to extract event information
+     */
     var _event = {
         target: function(evt) {
             var target = false;
@@ -44,12 +79,19 @@
         get_pos: function(evt) {
             var posx = 0;
             var posy = 0;
+            
             if (!e) var e = window.event;
-            if (e.pageX || e.pageY)   {
+
+            if(e.type == 'touchend' && e.changedTouches) {
+              touch = e.changedTouches[0];
+              posx = touch.clientX;
+              posy = touch.clientY;
+            }
+            else if (e.pageX || e.pageY)   {
               posx = e.pageX;
               posy = e.pageY;
             }
-            else if (e.screenX || e.screenY)  {
+            else if (e.clientX || e.clientY)  {
               posx = e.screenX;
               posy = e.screenY;
             }
@@ -62,15 +104,15 @@
         overlap: 'ui-overlap',
         dragged: 'ui-dragged',
         placeholder: 'ui-placeholder',
-        after_drag: function(elem) { 
-          console.log($(elem).html());
-          },
+        relative_to: '#game_play',
+        after_drag: false,
         on_drag: false,
         before_drag: false
     };
 
     var sortable = (function() {
         var sortable = function(containerEl, opts) {
+            this.opts = {};
             if (typeof containerEl === "string" || containerEl instanceof String) {
                 this.container = document.getElementById(containerEl);
             } else {
@@ -81,9 +123,14 @@
                 return;
             }
             if (this instanceof sortable) {
+                for (var j in defaults) {
+                    if (defaults.hasOwnProperty(j)) {
+                        this.opts[j] = defaults[j];
+                    }
+                }
                 for (var j in opts) {
                     if (opts.hasOwnProperty(j)) {
-                        this[j] = opts[j];
+                        this.opts[j] = opts[j];
                     }
                 }
             } else {
@@ -95,9 +142,6 @@
 
             jq(this.sortable).bind('destroy', function(e){
                 var id = that.sortable.jqmSortableId;
-                // window event need to be cleaned up manually, 
-                // remaining binds are automatically killed in 
-                // the dom cleanup process
                 if(cache[id]) delete cache[id];
                 e.stopPropagation();
             });
@@ -105,15 +149,29 @@
             // initial setup
             var $container=$(this.container);
             var data = Array.prototype.slice.call(this.container.childNodes);
-            while(data.length>0)
-            {
+            var relative_offset = false;
+
+            if(this.opts.relative_to) {
+              relative_offset = $(this.opts.relative_to).offset();
+            }
+
+            while(data.length>0) {
                 var myEl=data.splice(0,1);
                 myEl=$container.find(myEl)
                 if(!myEl.is('li'))
                    continue;
                 
                 var li_offset = myEl.offset();
+                if(relative_offset) { 
+                  margin = parseInt($(this.opts.relative_to).css('marginLeft'));
+                  li_offset.top = li_offset.top - relative_offset.top;
+                  li_offset.left = li_offset.left - relative_offset.left + margin;
+                }
+                
+                // add element to the main pool with offset information
                 this.li.push({elem: myEl, offset: li_offset}); 
+
+                // bind events
                 myEl.bind('touchmove', function(e) {that.drag(e);});
                 myEl.bind('touchend', function(e) {that.drag_end(e);});
                 myEl.bind('touchstart', function(e) {that.drag_start(e);});
@@ -122,29 +180,18 @@
                 $(document).bind('mouseup', function(e) {that.drag_end(e);});
                 myEl.bind('mousedown', function(e) {that.drag_start(e);});
             }
-
-
         };
 
         sortable.prototype = {
             dragging: false,
-
             offset: {x: 0, y: 0},
-
             overlay: -1,
-
             li:[],
-
-            default_options: {},
-
+            opts: {},
             placeholder: false,
             target: false,
             elem: false,
             elem_idx: -1,
-
-            log: function(message) {
-                $('#log').append('<li>'+message+'</li>');
-            },
 
             drag_start: function(evt) {
                 this.elem_idx = this.index(_event.get_pos(evt));
@@ -152,16 +199,16 @@
                     var that = this;
                     this.dragging = true;
                     this.elem = $(_event.target(evt));
-                    if(defaults.before_drag) {
-                        defaults.before_drag(this.elem, function() {
-                            $(that.elem).addClass(defaults.dragged);
+                    if(this.opts.before_drag) {
+                        this.opts.before_drag(this.elem, function() {
+                            $(that.elem).addClass(this.opts.dragged);
                             that.target = that.detach_elem(that.elem, evt);
                             that.create_placeholder(that.elem);
                         });
                     }
                     else
                     {
-                      $(this.elem).addClass(defaults.dragged);
+                      $(this.elem).addClass(this.opts.dragged);
                       this.target = this.detach_elem(this.elem, evt);
                       this.create_placeholder(this.elem);
                     }
@@ -169,20 +216,22 @@
             },
 
             drag_end: function(evt) {
-                var overlap = this.is_overlapping(this.target, pos)
+                var pos = _event.get_pos(evt);
+                var overlap = this.is_overlapping( pos)
                 var elem_changed = false;
+                var dragged_id = -1;
                 if(overlap >= 0) {
-                    this.change_elements(overlap);
+                    dragged_id = this.swap_elements(overlap);
                     var elem_changed = true;
                 }
                 this.attach_elem();
                 this.destroy_placeholder();
-                $('.'+defaults.overlap).removeClass(defaults.overlap);
-                $('.'+defaults.dragged).removeClass(defaults.dragged);
+                $('.'+this.opts.overlap).removeClass(this.opts.overlap);
+                $('.'+this.opts.dragged).removeClass(this.opts.dragged);
                 this.target = false;
                 this.dragging = false;
-                if(defaults.after_drag) {
-                    defaults.after_drag(this.li[overlap]);
+                if(this.opts.after_drag) {
+                    this.opts.after_drag(dragged_id, overlap);
                 }
 
             },
@@ -192,16 +241,16 @@
                     pos = _event.get_pos(evt);
                     target = _event.target(evt);
                     this.move_elem(this.target, pos.x, pos.y);
-                    var overlap = this.is_overlapping(this.target, pos)
+                    var overlap = this.is_overlapping(pos)
                     this.highligh_overlap(overlap);
                 }
             },
 
             highligh_overlap: function(idx) {
-              $('.'+defaults.overlap).removeClass(defaults.overlap);
+              $('.'+this.opts.overlap).removeClass(this.opts.overlap);
               if(idx >= 0)
               {
-                $(this.li[idx]['elem']).addClass(defaults.overlap);
+                $(this.li[idx]['elem']).addClass(this.opts.overlap);
               }
             },
 
@@ -213,44 +262,45 @@
                 elem_pos = $(elem).offset();
                 parent_pos = $(elem).parent().offset();
 
-                this.offset.x = pos.x - elem_pos.left;
-                this.offset.y = pos.y - elem_pos.top;
-
+                this.offset.x = pos.x - elem_pos.left + parent_pos.left ;
+                this.offset.y = pos.y - elem_pos.top + parent_pos.top;
+                
                 $(target).css({position: 'absolute'});
-
+                  
                 this.move_elem(target, pos.x, pos.y);
                 return target;
             },
 
-            change_elements: function(pos) {
+            swap_elements: function(pos) {
                 var idx = this.elem_idx;
                 if(idx < 0) return;
                 var dragged = this.li[idx]['elem'];
                 var overlap = this.li[pos]['elem'];
                 
+                $(dragged).removeClass('l' + idx).addClass('l' + pos);
+                $(overlap).removeClass('l' + pos).addClass('l' + idx);
+
                 this.li[idx]['elem'] = overlap;
                 this.li[pos]['elem'] = dragged;
 
                 $(dragged).insertBefore(overlap);
                 $(this.placeholder).insertBefore(overlap);
                 
-                if(idx == 0)
-                {
+                if(idx == 0) {
                     $(dragged).parent().prepend(overlap);
-                }
-                else
-                {
+                } else {
                     $(overlap).insertAfter(this.li[idx - 1]['elem']);
                 }
 
                 this.elem_idx = pos;
+
+                return idx;
             },
 
-            is_overlapping: function(elem, pos) {
+            is_overlapping: function(pos) {
                 var overlap = -1;
                 idx = this.index(pos);
-                if(idx >= 0 && idx != this.elem_idx)
-                {
+                if(idx >= 0 && idx != this.elem_idx) {
                   overlap = idx;
                 }
                 return overlap;
@@ -272,8 +322,8 @@
 
             move_elem: function(elem, x, y) {
                 elem_pos = $(elem).offset();
-                var real_x = x - this.offset.x;
-                    real_y = y - this.offset.y;
+                var real_x = Math.floor(x - this.offset.x);
+                    real_y = Math.floor(y - this.offset.y);
 
                 $(elem).css({top: real_y + 'px',
                              left: real_x + 'px'});
@@ -285,7 +335,7 @@
 
             create_placeholder: function(elem) {
                 var placeholder = $('<li>.</li>');
-                $(placeholder).addClass(defaults.placeholder);
+                $(placeholder).addClass(this.opts.placeholder);
                 $(placeholder).css({width: $(elem).width(),
                                     height: $(elem).height()});
 
@@ -307,5 +357,4 @@
 
         return sortable;
     })();
-
 })(jq);
